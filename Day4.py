50836ec64f95912bcaa6da13c8b6dc8ee37f9a2b
Sanskriti Oza <72627135+SanskritiOza@users.noreply.github.com>
2024-01-14 09:48:19 +0530

    Day4.py

---
+++ Day4.py
@@ -0,0 +1,10 @@
+def find_sum_of_elements(lst):
+    # Using the built-in sum() function to calculate the sum of elements in the list
+    total_sum = sum(lst)
+    return total_sum
+
+# Example usage:
+my_list = [1, 2, 3, 4, 5]
+result = find_sum_of_elements(my_list)
+
+print(f"The sum of elements in the list {my_list} is: {result}")
