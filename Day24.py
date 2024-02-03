924a813b74fe86936c933968260f7ead6c5a9640
Sanskriti Oza <72627135+SanskritiOza@users.noreply.github.com>
2024-02-03 16:19:16 +0530

    Day24.py

---
+++ Day24.py
@@ -0,0 +1,10 @@
+def remove_vowels(input_string):
+    vowels = "aeiouAEIOU"
+    result = ''.join(char for char in input_string if char not in vowels)
+    return result
+
+# Example usage:
+input_string = "Hello, World!"
+output_string = remove_vowels(input_string)
+print("Input String:", input_string)
+print("String without vowels:", output_string)
