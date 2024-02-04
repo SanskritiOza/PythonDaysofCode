4520ced70364ee3ca2d573e89e31c2310e30bdf4
Sanskriti Oza <72627135+SanskritiOza@users.noreply.github.com>
2024-02-04 11:27:13 +0530

    Day25.py

---
+++ Day25.py
@@ -0,0 +1,9 @@
+def concatenate_lists(list1, list2):
+    concatenated_list = list1 + list2
+    return concatenated_list
+
+# Example usage:
+list_a = [1, 2, 3]
+list_b = [4, 5, 6]
+result = concatenate_lists(list_a, list_b)
+print(result)
