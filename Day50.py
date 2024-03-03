605638410fd0e2a3f5f8d74452a7d34e049c9e9c
Sanskriti Oza <72627135+SanskritiOza@users.noreply.github.com>
2024-03-03 10:33:00 +0530

    Day50.py

---
+++ Day50.py
@@ -0,0 +1,14 @@
+def main():
+    set1 = {1, 2, 3, 4, 5}
+    set2 = {4, 5, 6, 7, 8}
+
+    # Intersection of two sets
+    intersection = set1.intersection(set2)
+    print("Intersection of set1 and set2:", intersection)
+
+    # Union of two sets
+    union = set1.union(set2)
+    print("Union of set1 and set2:", union)
+
+if __name__ == "__main__":
+    main()
